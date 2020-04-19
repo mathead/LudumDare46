38176e864f77731796e9304af43a3c5e93c3cbe6
Filip Volf <volffili@fit.cvut.cz>
2020-04-19 21:23:31 +0100

    refactor

---
+++ scenes/main.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var knight = preload("res://scenes/knight.tscn")
+var width
+var height
+
+func _ready():
+	width = get_viewport_rect().size.x
+	height = get_viewport_rect().size.y
+	generate_knights()
+
+func generate_knights():
+	var left_side = width/4
+	var right_side = width/4*3
+	
+	for y in range(0,height,24):
+		var left_knight = knight.instance()
+		left_knight.position = Vector2(left_side,y)
+
+		var right_knight = knight.instance()
+		right_knight.position = Vector2(right_side,y)
