feab96ea71e4545576efd7d623455c0258c4ac2f
Filip Volf <volffili@fit.cvut.cz>
2020-04-20 12:00:23 +0100

    Fighting with letter 'G'

--- scenes/player.gd
+++ scenes/player.gd
@@ -6,6 +6,7 @@ var motion = Vector2.ZERO
 
 onready var spr = $Sprite
 var spr_offset = 0
+var hitting = false
 
 var mus_path = "res://sprites/musician/"
 var spr_frames = []
@@ -14,6 +15,7 @@ func _ready():
 	for instr in ["bagpipes","drum","harp","lute","trumpet"]:
 		spr_frames.append(load(mus_path + instr + "/idle.tres"))
 		spr_frames.append(load(mus_path + instr + "/run.tres"))
+		spr_frames.append(load(mus_path + instr + "/fight.tres"))
 	take_bagpipes()
 
 func _physics_process(delta):
@@ -26,16 +28,21 @@ func _physics_process(delta):
 	
 func get_input_axis():
 	var axis = Vector2.ZERO
+	
+	var fight = Input.is_action_pressed("ui_fight")
 	var right = Input.is_action_pressed("ui_right")
 	var left = Input.is_action_pressed("ui_left")
 	
 	axis.x = int(right)-int(left)
 	axis.y = int(Input.is_action_pressed("ui_down"))-int(Input.is_action_pressed("ui_up"))
 	
-	if axis.length() > 0:
-		_anim_walk()
+	if fight:
+		_anim_fight()
 	else:
-		_anim_idle()
+		if axis.length() > 0:
+			_anim_walk()
+		else:
+			_anim_idle()
 	
 	if right:
 		spr.flip_h = true
@@ -71,19 +78,16 @@ func take_trumpet():
 	spr_offset = 4
 
 	
-func _anim_fight():
-	pass
-	
 func _anim_idle():
-	print("TRYING IDLING")
 	_change_anim(0)
 	
 func _anim_walk():
-	print("TRYING WALKING")
 	_change_anim(1)
 	
+func _anim_fight():
+	_change_anim(2)
+	
 func _change_anim(offset):	
-	var frames = spr_frames[2*spr_offset+offset]
+	var frames = spr_frames[3*spr_offset+offset]
 	if spr.frames != frames:
-		print("CAU")
 		spr.frames = frames
