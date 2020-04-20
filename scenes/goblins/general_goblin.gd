2daa7dd82966585573fe779e0957eb386c708c46
Filip Volf <volffili@fit.cvut.cz>
2020-04-20 19:26:10 +0100

    General goblin and other goblins should be inherited

--- scenes/goblins/general_goblin.gd
+++ scenes/goblins/general_goblin.gd
@@ -1,16 +1,16 @@
 extends AnimatedSprite
 
-export var health = 10.0 
-export var running_speed = 20.0
+export var health:int
+export var running_speed:float
 export(int, "shield","sword","spear","bow") var counter_weapon
 export var knockback_amount:float
-
 
 func _process(delta):
 	position += Vector2(1.0,0)*delta*running_speed*scale.x
 
 func _on_Area2D_body_entered(body):
-	print(body.get_name())
+	if body.is_in_group("Knights"):
+		queue_free()
 
 func turn_around():
 	scale.x *= -1
