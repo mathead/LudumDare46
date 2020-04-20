cb06d3f5b891296114dcac4e1a0b0bfea4762855
Filip Volf <volffili@fit.cvut.cz>
2020-04-20 15:12:51 +0100

    Goblin more properties

--- scenes/goblin.gd
+++ scenes/goblin.gd
@@ -2,10 +2,11 @@ extends AnimatedSprite
 
 export var health:int
 export var running_speed:float
+export(int, "shield","sword","spear","bow") var counter_weapon
+export var knockback_amount:float
 
 func _ready():
 	pass
-
 
 func _process(delta):
 	position.x += delta*running_speed
